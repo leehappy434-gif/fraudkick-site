7fce5a8b686f2bce90366ba03c0bb7cc18cb2c2a
Ben <happylee434@gmail.com>
2025-12-18 23:30:08 +0800

    Updates

--- _next/static/WaQMnBVqDemsJfctq1RIq/_buildManifest.js
+++ _next/static/WaQMnBVqDemsJfctq1RIq/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fraudkick-site//_next/:path+",
+        "destination": "/fraudkick-site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
